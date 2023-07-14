e7b1bcdb9c8ccf581f86f57e5532f21c31f05b40
Edward Yang <edward_yang_125@hotmail.com>
2023-07-14 17:07:39 +1000

    Add strain calculation

--- classes.py
+++ classes.py
@@ -20,6 +20,7 @@ class particles:
         self.rho = np.full(maxn, rho_ini)
         self.id = np.arange(maxn)
         self.type = np.ones(maxn)
+        self.strain = np.zeros((maxn, 4))
         self.sigma = np.zeros((maxn, 4))
 
         self.pairs = np.ndarray((maxinter, 2))
@@ -44,13 +45,18 @@ class particles:
         tree = sp.spatial.cKDTree(self.x[0:self.ntotal+self.nvirt, :])
         self.pairs = tree.query_pairs(3*self.dx, output_type='ndarray')
 
-    def pair_sweep(self, dvdt: np.ndarray, drhodt: np.ndarray, kernel: typing.Type):
+    def pair_sweep(self, 
+                   dvdt: np.ndarray, 
+                   drhodt: np.ndarray, 
+                   dstraindt: np.ndarray, 
+                   kernel: typing.Type):
 
         for i, j in self.pairs:
             dx = self.x[i, :] - self.x[j, :]
             dwdx = kernel.dwdx(dx)
 
-            vr = np.dot(self.v[i, :]-self.v[j, :], dx[:])
+            dv = self.v[i, :] - self.v[j, :]
+            vr = np.dot(dv[:], dx[:])
             if vr > 0.: vr = 0.
             rr = np.dot(dx[:], dx[:])
             muv = self.h*vr/(rr + self.h*self.h*0.01)
@@ -74,6 +80,22 @@ class particles:
             drhodt[i] += tmp_drhodt
             drhodt[j] += tmp_drhodt
 
+            he = np.zeros(4)
+            he[0] = -dv[0]*dwdx[0]
+            he[1] = -dv[1]*dwdx[1]
+            #he[2] = 0.
+            he[3] = -0.5*(dv[0]*dwdx[1]+dv[1]*dwdx[0])
+
+            dstraindt[i, 0] += self.mass*he[0]/self.rho[j]
+            dstraindt[i, 1] += self.mass*he[1]/self.rho[j]
+            # dstraindt[i, 2] += self.mass*he[2]/self.rho[j]
+            dstraindt[i, 3] += self.mass*he[3]/self.rho[j]
+
+            dstraindt[j, 0] += self.mass*he[0]/self.rho[i]
+            dstraindt[j, 1] += self.mass*he[1]/self.rho[i]
+            # dstraindt[j, 2] += self.mass*he[2]/self.rho[i]
+            dstraindt[j, 3] += self.mass*he[3]/self.rho[i]
+
     def save_data(self, itimestep: int):
 
         with h5py.File(f'output/sph_{itimestep}.h5', 'w') as f:
@@ -82,6 +104,7 @@ class particles:
             f.create_dataset("type", data=self.type[0:self.ntotal+self.nvirt], dtype="i", compression="gzip")
             f.create_dataset("rho", data=self.rho[0:self.ntotal+self.nvirt], dtype="f8", compression="gzip")
             f.create_dataset("sigma", data=self.sigma[0:self.ntotal+self.nvirt, :], dtype="f8", compression="gzip")
+            f.create_dataset("strain", data=self.strain[0:self.ntotal+self.nvirt, :], dtype="f8", compression="gzip")
         
 class integrators:
     def __init__(self,
@@ -104,6 +127,8 @@ class integrators:
         v0 = np.empty((pts.ntotal+pts.nvirt, 2))
         drhodt = np.zeros(pts.ntotal+pts.nvirt)
         rho0 = np.empty(pts.ntotal+pts.nvirt)
+        dstraindt = np.zeros((pts.ntotal+pts.nvirt, 4))
+        sigma0 = np.empty((pts.ntotal+pts.nvirt, 4))
 
         dt = self.cfl*pts.dx*3./pts.c
 
@@ -111,26 +136,31 @@ class integrators:
 
             pts.findpairs()
 
-            v0 = np.copy(pts.v)
-            rho0 = np.copy(pts.rho)
+            v0 = np.copy(pts.v[0:pts.ntotal+pts.nvirt, :])
+            rho0 = np.copy(pts.rho[0:pts.ntotal+pts.nvirt])
+            sigma0 = np.copy(pts.sigma[0:pts.ntotal+pts.nvirt, :])
 
             for i in range(pts.ntotal+pts.nvirt):
                 pts.rho[i] += 0.5*dt*drhodt[i]
                 if pts.type[i] > 0:
                     pts.v[i, :] += 0.5*dt*dvdt[i, :]
+                #pts.stress_update(i, 0.5*dt*dstraindt[i, :], sigma0[i, :])
 
             dvdt = np.tile(self.f, (pts.ntotal+pts.nvirt, 1))
             drhodt = np.zeros(pts.ntotal+pts.nvirt)
+            dstraindt = np.zeros((pts.ntotal+pts.nvirt, 4))
 
             pts.stress_update()
             
-            pts.pair_sweep(dvdt, drhodt, self.kernel)
+            pts.pair_sweep(dvdt, drhodt, dstraindt, self.kernel)
 
             for i in range(pts.ntotal+pts.nvirt):
                 pts.rho[i] = rho0[i] + dt*drhodt[i]
                 if pts.type[i] > 0:
                     pts.v[i, :] = v0[i, :] + dt*dvdt[i, :]
-                    pts.x[i, :] = pts.x[i, :] + dt*pts.v[i, :]
+                    pts.x[i, :] += dt*pts.v[i, :]
+                #pts.stress_update(i, dt*dstraindt[i, :], sigma0[i, :])
+                pts.strain[i, :] += dt*dstraindt[i, :]
 
             if itimestep % self.printtimestep == 0:
                 print(f'time-step: {itimestep}')
