98f943a8a533badaf258833f4b73cfe24824c0c0
Edward Yang <yang.e@wehi.edu.au>
2023-09-30 08:36:32 +1000

    Revert "Directly reference numpy stuff"
    This reverts commit 4fd1920f23888312896931756520bf1d6a73e9d5.

--- kernels.py
+++ kernels.py
@@ -1,15 +1,13 @@
 import numpy as np
-from numpy import sqrt, pi
 
 class wenland_c2:
     def __init__(self, k, h):
         self.k = k
         self.h = h
-        self.alpha = 7./(64.*pi*h*h)
+        self.alpha = 7./(64.*np.pi*self.h*self.h)
     def w(self, r: float):
         q = r/self.h
         return self.alpha*max(0., 2.-q)**4*(2.*q+1.)
     def dwdx(self, dx: np.ndarray):
-        h = self.h
-        q = sqrt(dx[0]*dx[0]+dx[1]*dx[1])/h
-        return -self.alpha*10.*max(0., 2.-q)**3*dx[0:2]/(h*h)
+        q = np.sqrt(dx[0]*dx[0]+dx[1]*dx[1])/self.h
+        return -self.alpha*10.*max(0., 2.-q)**3*dx[:]/(self.h*self.h)
