63ad01c6a28d9e10e97641a72acb9ff738b3f64f
Edward <edward_yang_125@hotmail.com>
2023-10-14 20:30:50 +1100

    add wendland_c2 correctness test

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -32,7 +32,7 @@ def test_kernel_methods():
     for cls in kernel_classes:
         kernel = init_kernel(cls)
         try:
-            w = kernel.w(np.array([0.5]))
+            w = kernel.w(0.5)
             dwdx = kernel.dwdx(np.array([[0.5, 0.5]]))
         except AttributeError:
             assert False, f"{cls.__name__} doesn't have w or dwdx functions."
@@ -40,3 +40,18 @@ def test_kernel_methods():
             assert False, f"{cls.__name__} w or dwdx functions don't accept the correct inputs."
 
     assert True
+
+def test_wendlandc2_correctness():
+    dx = np.array([[0.5, 0.5]])
+    r = np.linalg.norm(dx[0])
+    h = 1.
+    q = r/h
+    alpha = 7./(64.*np.pi*h*h)
+    w_ref = alpha*np.maximum(0., 2.-q)**4*(2.*q+1.) # known correct calculation
+    dwdx_ref = -alpha*10.*q*np.maximum(0., 2.-q)**3/(r*h)*dx
+    kernel = init_kernel(pygrasph.kernels.wendland_c2)
+    
+    res = w_ref == kernel.w(r)
+    res = res and np.all(dwdx_ref==kernel.dwdx(dx))
+
+    assert res
